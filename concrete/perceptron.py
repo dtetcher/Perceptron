1bdc2a6fad8189ea292bdcd5f0945bd6b0071c3e
Arthur <drabazha.arthur@gmail.com>
2020-09-22 15:25:19 +0300

    Possible to turn on/off 'weights stuck' condition

--- concrete/perceptron.py
+++ concrete/perceptron.py
@@ -1,11 +1,28 @@
 from typing import List
 
+from abstract.activation import ActivationFunction as AFunc
 from abstract.perceptron import AbstractPerceptron
 from concrete.checkers import WeightsAnalyser
 
 
 class Perceptron(AbstractPerceptron):
-    def _train(self, laps: int):
+
+    def __init__(self, input_data: List[list],
+                 output_layer_pattern,
+                 activation_function: AFunc,
+                 stuck_condition: bool = True,
+                 min_weight_val: float = -0.5,
+                 max_weight_val: float = 0.5,
+                 training_speed: float = 0.05,
+                 config_file: str = './dump.json',
+                 ):
+
+        self.__stuck_condition = stuck_condition
+
+        super().__init__(input_data, output_layer_pattern, activation_function,
+                         min_weight_val, max_weight_val, training_speed, config_file)
+
+    def _train(self, laps: int, ):
 
         analyzer = WeightsAnalyser(len(self._input_data))
 
@@ -16,11 +33,12 @@ class Perceptron(AbstractPerceptron):
                 if not neuron_status:
                     self._backward_propagation(sample, self._output_data[i], neuron_status)
 
-                analyzer.push(self.synaptic_weights)
-                if analyzer.is_stable():
-                    return lap
+                if self.__stuck_condition:
+                    analyzer.push(self.synaptic_weights)
+                    if analyzer.is_stable():
+                        return lap
 
-        return lap
+        return laps
 
     def test_data_sample(self, dataset: List[list]):
         return [self._activation_F(self._SUM(d)) for d in dataset]
--- main.py
+++ main.py
@@ -13,18 +13,19 @@ def main():
         [0, 1, 1],
         [1, 1, 1]
     ]
-    training_speed = 0.002
+    training_speed = 0.2
 
     p = Perceptron(input_data=input_data,
                    output_layer_pattern=lambda x: x[0] and (not x[1] or x[2]),
                    activation_function=Threshold(),
                    training_speed=training_speed,
+                   stuck_condition=True
                    )
 
     p.load()
 
     p.begin_training(laps=200_000)
-    p.dump()
+    # p.dump()
 
 
 if __name__ == '__main__':
